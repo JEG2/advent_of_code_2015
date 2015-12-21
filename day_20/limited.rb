8a012c994073a7433d6df2e99f404976840a9c2f
James Edward Gray II <james@graysoftinc.com>
2015-12-21 09:57:47 -0600

    Resolving this one myself.

--- day_20/limited.rb
+++ day_20/limited.rb
@@ -1,16 +1,15 @@
 #!/usr/bin/env ruby -w
 
-# modified from:  https://www.reddit.com/r/adventofcode/comments/3xjpp2/day_20_solutions/cy5abc3
+GOAL = 34_000_000
 
-target = 34_000_000
-size = target / 10
-counts = Array.new(size, 0)
-1.upto(size) do |n|
-  counter = 0
-  n.step(by: n, to: size - 1) do |i|
-    counts[i] += 11 * n
-    counter += 1
-    break if counter == 50
+houses = Array.new(GOAL + 1, 0)
+1.upto(GOAL) do |elf|
+  presents = elf * 11
+  1.upto(50) do |offset|
+    house = elf * offset
+    break if house > GOAL
+    houses[house] += presents
   end
 end
-p counts.index { |c| c >= target }
+
+p houses.index { |house| house >= GOAL }
